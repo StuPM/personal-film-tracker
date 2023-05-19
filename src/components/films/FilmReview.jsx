e6b7fa3f56ea3a86c59e7d48e972908be6e747ca
StuPM <stuartpmcgee@gmail.com>
2023-05-19 13:07:10 +0100

    Review component reworked to get data from database.

--- src/components/films/FilmReview.jsx
+++ src/components/films/FilmReview.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { useEffect } from "react";
+import api from "../../api";
+import { useState } from "react";
 
-const FilmReview = ({ review }) => {
+const FilmReview = ({ id }) => {
+  const [reviews, setReviews] = useState();
+
+  useEffect(() => {
+    async function getReviews() {
+      const result = await api("GETREVIEWSBYID", { id: id });
+      setReviews(result);
+      console.log(result);
+    }
+    getReviews();
+  }, []);
+
   return (
     <div className="individualReview">
-      <p>{review.id}</p>
+      <p>testing</p>
+      {/* <p>{review.id}</p>
       <p>{review.rating}</p>
       <p>When: {review.viewingDate}</p>
       <p>Where : {review.location === 1 ? "Cinema" : "Home"}</p>
-      <p>{review.review}</p>
+      <p>{review.review}</p> */}
     </div>
   );
 };
